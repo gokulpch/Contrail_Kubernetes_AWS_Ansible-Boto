93c67efbf6e35c983579e3702fcbaf25a5393daa
Gokul Chandra <gokulpch@bu.edu>
2017-06-03 04:03:18 -0700

    Key_ID change

--- create_key_pair.sh
+++ create_key_pair.sh
@@ -9,7 +9,7 @@ echo "Running Ansible playbook to create a Keypair..............."
 echo ""
 echo ""
 echo "**************************************************************************************"
-ansible-playbook /root/Kubernetes_AWS_Flannel_Ansible-BOTO/ansible/create_keypair.yml
+ansible-playbook /root/Contrail_Kubernetes_AWS_Ansible-Boto/ansible/create_keypair.yml
 echo "Copying Keypair.pem to local machine..............."
 eval `ssh-agent -s`;
 ssh-add ~/.ssh/contrailK8S-keypair.pem;
