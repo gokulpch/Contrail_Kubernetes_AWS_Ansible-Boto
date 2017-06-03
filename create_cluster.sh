995e5043b59bcd84fce5c25ac29f8c9302474069
Gokul Chandra <gokulpch@bu.edu>
2017-06-03 04:07:22 -0700

    Update create_cluster.sh

--- create_cluster.sh
+++ create_cluster.sh
@@ -5,4 +5,4 @@
 ######
 ############################################################################
 echo "Running ansible to create K8S cluster................."
-ansible-playbook ./ansible/create_cluster.yml -v
+ansible-playbook /root/Contrail_Kubernetes_AWS_Ansible-Boto/ansible/create_cluster.yml -v
